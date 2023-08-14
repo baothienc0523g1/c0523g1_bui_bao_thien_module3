57a4cde620d572dac8880a7913fe29109f62af3a
baothienc0523g1 <noimandembuongxuong@gmail.com>
2023-08-14 11:06:02 +0700

    bai tap ss3

---
+++ ss03/exercise/bai_tap2.sql
@@ -0,0 +1,97 @@
+create database ss03_bai2;
+use ss03_bai2;
+
+create table customers(
+customer_id int not null primary key auto_increment,
+customer_name varchar(55),
+customer_age int
+);
+
+create table orders(
+order_id int not null primary key auto_increment,
+customer_id int,
+foreign key (customer_id) references customers(customer_id),
+order_day datetime,
+order_total_price double
+);
+
+create table products(
+product_id int not null primary key auto_increment,
+product_name varchar(55),
+product_price double
+);
+
+create table order_detail(
+order_id int,
+foreign key (order_id) references orders(order_id),
+product_id int,
+foreign key (product_id) references products(product_id),
+order_quantity int,
+primary key(order_id, product_id)
+);
+
+insert into customers(customer_name,customer_age)
+values
+("Minh Quan", 10),
+("Ngoc Oanh", 20),
+("Hong Ha", 50);
+
+insert into orders(customer_id,order_day)
+values
+(1, "2006-03-21"),
+(2, "2006-03-23"),
+(1, "2006-03-16");
+
+insert into products(product_name,product_price)
+values 
+("May Giat", 3),
+("Tu Lanh",5),
+("Dieu Hoa",7),
+("Quat",1),
+("Bep Dien",2);
+
+insert into order_detail(order_id,product_id,order_quantity)
+values
+(1,1,3),
+(1,3,7),
+(1,4,2),
+(2,1,1),
+(3,1,8),
+(2,5,4),
+(2,3,3);
+
+
+
+/*Hiển thị các thông tin  gồm 
+oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order*/
+select * from orders;
+
+/*Hiển thị danh sách các khách hàng đã mua hàng,
+ và danh sách sản phẩm được mua bởi các khách*/
+ select c.customer_id, c.customer_name, p.product_name
+ from customers as c
+ join orders as o 
+ on c.customer_id = o.customer_id
+ join order_detail as detail 
+ on o.order_id = detail.order_id
+ join products as p 
+ on detail.product_id = p.product_id;
+ 
+ /*Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào*/
+select c.customer_id,c.customer_name
+from customers as c
+left join orders as o
+on c.customer_id = o.customer_id
+where o.customer_id is null;
+ 
+ /*Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn 
+ (giá một hóa đơn được tính bằng tổng giá bán của
+ từng loại mặt hàng xuất hiện trong hóa đơn. 
+ Giá bán của từng loại được tính = odQTY*pPrice)*/
+ select detail.order_id, o.order_day, p.product_name,order_quantity,
+ (order_quantity * product_price) as total_price
+ from order_detail as detail
+ right join orders as o
+ on detail.order_id = o.order_id
+ join products as p
+ on detail.product_id = p.product_id;
--- ss03/exercise/bai_tap_them.sql
+++ ss03/exercise/bai_tap_them.sql
@@ -4,32 +4,49 @@ use ss2;
 /*1*/
 select *
 from student as s
-left join class as c
-on s.class_id = c.id
-where class_id is not null;
+join class as c
+on s.class_id = c.id;
 
 /*2*/
 select * 
 from student as s
 left join class as c
 on s.class_id = c.id;
-select * from student; 
 
 /*4*/
-select * from student where (`name` like "%Hai" or `name` like "%Huynh"); 
+select * from student
+where (`name` like "% Hai" or `name` like "% Huynh"); 
 
 /*5*/
-select * from student where (`point` > 5);
+select * from student
+where (`point` > 5)
+order by `point`;
 
 /*6*/
-select * from student where (`name` like "Nguyen%");
+select * from student where (`name` like "Nguyen %");
 
 /*7*/
-select `point`, `name` from student group by `name`;
+select `point`, count(*) as "So luong"
+from student group by `point` order by `point`;
 
 /*8*/
-select * from student  where (`point` > 5) group by `point` order by `point` ; 
-select count(*) from student where (`point` > 5);
+select `point`, count(*) as "So luong"
+from student
+where (`point` > 5) 
+group by `point`
+order by `point` ; 
+
 /*9*/
+select `point`, count(*) as "So luong"
+from student
+where `point` > 5
+group by `point`
+having count(*) >= 2
+order by `point`;
 
 /*10*/
+select *
+from student as s
+join class as c
+where c.name = "C1121G1"
+ORDER BY substring_index(s.name,' ', -1);
